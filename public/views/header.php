193b67035828c28453d9a3ef0aff2e0f57f72466
Yanik-Dev <yanikblake@live.com>
2017-04-13 12:28:04 -0500

    added header.php

---
+++ public/views/header.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+      <title>Garden Of Paradise - Home </title>
+      <link rel="stylesheet" type="Text/css" href="../css/style.css">
+    </head>
+    <body>
+    <div class="header">
+        <div class ="social-bar">
+        </div>
+        <div class="header-logo">
+        </div>
+    </div>
+
+</body>
+</html>
