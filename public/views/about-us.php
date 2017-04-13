600c8d20fcf236a93c241b18c0e383203858c209
DivDennis <divia.deidre@outlook.com>
2017-04-13 14:17:46 -0500

    added us.css

--- public/views/about-us.php
+++ public/views/about-us.php
@@ -1,11 +1,59 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/us.css">
+</head>
 <body>
     <div class="container">
       <div class="us-1">
+        <img id="side-img" src="../img/about-us-casket.jpg">
 
+        <p>
+          At Garden of Paradise Funeral Home, we pride ourselves on being talented which is evident in every job we complete. Our level of professionalism is par excellence!!
+        </p>
+        <br/>
+        <br/>
+        <div class="contact">
+          <img src="../img/real-estate.png">
+          <p>1 North Avenue, Kingston 5 </p>
+        </div>
+        <div class="contact">
+          <img src="../img/phone-receiver.png">
+          <p>Lime: (1-876) 327-9729</p>
+        </div>
+        <div class="contact">
+          <img src="../img/phone-receiver.png">
+          <p>Digicel: (1-876) 870-5848</p>
+        </div>
       </div>
       <div class="us-2">
-
+        <h1>Welcome</h1>
+        <p>Garden of Paradise Funeral Home was established in 2006 by Sweets Lawrence (CEO/Funeral Director) and incorporated by
+          The Office of the Registrar of Companies in Kingston, Jamaica on the 17th of February 2007;
+          one year after the death of Francine Vesna Dillon to whose memory the company was dedicated.
+        </p>
+        <p>
+          Her passion for the business started after losing her grandmother to cancer at the tender age of 10 and due to her age she
+          wasn’t allowed to view her (grand-mother) being removed from their home. As a result of that she made it her point of duty every weekend to purchase
+          “The Sunday Gleaner” when possible to focus solely on the death columns.
+        </p>
+        <p>
+          Sweets has been working in the funeral industry for over 6
+          years before opening her own company creating the ability to offer and spread her passion with care and comfort to all her clients.
+        </p>
+        <p>
+          Ms. Lawrence currently holds a Diploma in Funeral Services. She is certified by the Stratford University in Canada.
+          Garden of Paradise Funeral Home is currently operated by Mark Maitland,
+          JP who is the current General Manager. Mr. Maitland continues to build on the legacy Ms. Lawrence has left.
+        </p>
+        <p>
+          Ours is a service oriented organization, catering to persons when they most need comfort, support and understanding.
+          Though a relatively young business, we have handled routine as well as challenging cases, always delivering the same quality product to our clients.
+        </p>
+        <p>
+          We stand by our Motto: “Granting relief in time of grief."
+        </p>
       </div>
     </div>
 </body>
+</html>
