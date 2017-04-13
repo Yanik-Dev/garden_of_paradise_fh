1378d2add142a78e63703ee35821991a22f52ac5
Yanik-Dev <yanikblake@live.com>
2017-04-13 15:16:06 -0500

    added bootstrap to project

--- public/views/about-us.php
+++ public/views/about-us.php
@@ -2,7 +2,7 @@
 <body>
     <div class="container">
       <div class="us-1">
-
+      <p>this</p>
       </div>
       <div class="us-2">
 
--- public/views/header.php
+++ public/views/header.php
@@ -1,15 +1,23 @@
+<!doctype html>
 <html>
     <head>
       <title>Garden Of Paradise - Home </title>
       <link rel="stylesheet" type="Text/css" href="../css/style.css">
+      <link rel="stylesheet" type="Text/css" href="../lib/bootstrap/css/bootstrap.min.css">
+      
+   <script type="text/JavaScript" src="../lib/jquery/jquery-3.2.1.min.js"></script>
+   <script type="text/JavaScript" src="../lib/bootstrap/js/bootstrap.min.js"></script>
     </head>
     <body>
     <div class="header">
-        <div class ="social-bar">
-        </div>
-        <div class="header-logo">
+        <div class ="container">
+            <a href="#"><img src="../img/icons/gmail.svg" class="social-icon" alt=""> </a>
+            <a href="#"><img src="../img/icons/facebook.svg" class="social-icon" alt=""> </a>
+            <a href="#"><img src="../img/icons/twitter.svg" class="social-icon" alt=""></a>
+            <a href="#"><img src="../img/icons/youtube.svg" class="social-icon" alt=""></a>
+            <a href="#" id="location"> 
+                <img src="../img/icons/youtube.svg" alt="">
+                <span>1 North Avenue, Kingston 5</span>
+            </a>
         </div>
     </div>
-
-</body>
-</html>
