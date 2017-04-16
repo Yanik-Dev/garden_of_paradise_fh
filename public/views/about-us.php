46234339d533b30ac5b00aec4ee26361a34f2dd8
DivDennis <divia.deidre@outlook.com>
2017-04-16 15:46:46 -0500

    Completed About-Us and Contact-Us Page.

--- public/views/about-us.php
+++ public/views/about-us.php
@@ -1,55 +1,75 @@
-<?php include('./header.php'); ?>
-    <div class="container-fiuld">
-      <div class="us-1">
-        <img id="side-img" src="../img/about-us-casket.jpg">
 
-        <p>
-          At Garden of Paradise Funeral Home, we pride ourselves on being talented which is evident in every job we complete. Our level of professionalism is par excellence!!
-        </p>
-        <br/>
-        <br/>
-        <div class="contact">
-          <img src="../img/real-estate.png">
-          <p>1 North Avenue, Kingston 5 </p>
-        </div>
-        <div class="contact">
-          <img src="../img/phone-receiver.png">
-          <p>Lime: (1-876) 327-9729</p>
-        </div>
-        <div class="contact">
-          <img src="../img/phone-receiver.png">
-          <p>Digicel: (1-876) 870-5848</p>
-        </div>
-      <p>this</p>
-      </div>
-      <div class="us-2">
-        <h1>Welcome</h1>
-        <p>Garden of Paradise Funeral Home was established in 2006 by Sweets Lawrence (CEO/Funeral Director) and incorporated by
-          The Office of the Registrar of Companies in Kingston, Jamaica on the 17th of February 2007;
-          one year after the death of Francine Vesna Dillon to whose memory the company was dedicated.
-        </p>
-        <p>
-          Her passion for the business started after losing her grandmother to cancer at the tender age of 10 and due to her age she
-          wasn’t allowed to view her (grand-mother) being removed from their home. As a result of that she made it her point of duty every weekend to purchase
-          “The Sunday Gleaner” when possible to focus solely on the death columns.
-        </p>
-        <p>
-          Sweets has been working in the funeral industry for over 6
-          years before opening her own company creating the ability to offer and spread her passion with care and comfort to all her clients.
-        </p>
-        <p>
-          Ms. Lawrence currently holds a Diploma in Funeral Services. She is certified by the Stratford University in Canada.
-          Garden of Paradise Funeral Home is currently operated by Mark Maitland,
-          JP who is the current General Manager. Mr. Maitland continues to build on the legacy Ms. Lawrence has left.
-        </p>
-        <p>
-          Ours is a service oriented organization, catering to persons when they most need comfort, support and understanding.
-          Though a relatively young business, we have handled routine as well as challenging cases, always delivering the same quality product to our clients.
-        </p>
-        <p>
-          We stand by our Motto: “Granting relief in time of grief."
-        </p>
-      </div>
+<link rel="stylesheet" href="../css/us.css">
+  
+  <body>
+    <div class="us-container">
+    <div class="us-1">
+      <img class="casket" src="../img/about-us-casket.jpg" align="middle">
+       <p>
+         At Garden of Paradise Funeral Home, we pride ourselves on being talented which is evident in every job we complete. Our level of professionalism is par excellence!!
+       </p>
+       <div class="address">
+         <img src="../img/icons/real-estate.png" alt="Address">
+         <p>1 North Avenue, Kingston 5</p>
+       </div>
+       <br>
+       <br>
+       <br>
+       <br>
+       <div class="contact-1">
+         <img src="../img/icons/phone-receiver.png" alt="Phone-Lime">
+         <p>Lime: (1-876) 327-9729</p>
+       </div>
+       <br>
+       <br>
+       <br>
+       <div class="contact-2">
+         <img src="../img/icons/phone-receiver.png" alt="Phone-Digicel">
+         <p>Digicel: (1-876) 870-5848</p>
+       </div>
+       <br>
+       <br>
+       <br>
+       <div class="contact-3">
+         <img src="../img/icons/email.png" alt="Email">
+         <p>	gardenofparadisefh@gmail.com</p>
+       </div>
     </div>
-</body>
+    <div class="us-2">
+      <h1>Welcome</h1>
+      <p class="par-1">
+        Garden of Paradise Funeral Home was established in 2006 by Sweets Lawrence (CEO/Funeral Director) and incorporated by
+            The Office of the Registrar of Companies in Kingston, Jamaica on the 17th of February 2007;
+            one year after the death of Francine Vesna Dillon to whose memory the company was dedicated.
+          </p>
+          <p class="par-2">
+            Her passion for the business started after losing her grandmother to cancer at the tender age of 10 and due to her age she
+            wasn’t allowed to view her (grand-mother) being removed from their home. As a result of that she made it her point of duty every weekend to purchase
+            “The Sunday Gleaner” when possible to focus solely on the death columns.
+          </p>
+          <p class="par-2">
+            Sweets has been working in the funeral industry for over 6
+            years before opening her own company creating the ability to offer and spread her passion with care and comfort to all her clients.
+          </p>
+          <p class="par-2">
+            Ms. Lawrence currently holds a Diploma in Funeral Services. She is certified by the Stratford University in Canada.
+            Garden of Paradise Funeral Home is currently operated by Mark Maitland,
+            JP who is the current General Manager. Mr. Maitland continues to build on the legacy Ms. Lawrence has left.
+          </p>
+          <p class="par-2">
+            Ours is a service oriented organization, catering to persons when they most need comfort, support and understanding.
+            Though a relatively young business, we have handled routine as well as challenging cases, always delivering the same quality product to our clients.
+          </p>
+          <p class="par-2">
+            The staff of Garden of Paradise Funeral Home takes this concept seriously – a funeral is a time to honor the life of the deceased and celebrate the heritage of their family. We strive to make each funeral a respectful,
+            fulfilling experience that meets the unique needs of each family.
+          </p>
+          <p class="par-2">
+            We stand by our Motto: “Granting relief in time of grief."
+          </p>
+    </div>
+    </div>
+
+
+  </body>
 </html>
---
+++ public/views/contact-us.php
@@ -0,0 +1,65 @@
+<link rel="stylesheet" href="../css/us.css">
+
+<body>
+    
+    <div class="contact-container">
+
+
+    <div class="contact-us-1">    
+    <div class="contact-email">
+        <img src="../img/icons/email-contact.png" alt="Email">
+        <br>
+        <br>
+        <center>
+        <p>gardenofparadisefh@gmail.com</p>
+        <p>www.gardenofparadisefh.com</p>
+        </center>
+    </div>
+
+    <div class="contact-phone">
+        <img src="../img/icons/telephone-contact.png" alt="Phone">
+        <br>
+        <br>
+        <center>
+        <p>(1-876) 327-9729</p>
+        <p>(1-876) 870-5848</p>
+        </center>
+    </div>
+
+    <div class="contact-location">
+        <img src="../img/icons/location-contact.png" alt="Location">
+        <br>
+        <br>
+       <center>
+        <p>1 North Avenue, Kingston 5</p>
+        <p>Jamaica</p>
+        </center>
+    </div>
+    </div>
+    <br>
+    <div class="contact-us-2">
+
+        <h1>CONTACT US TODAY</h1>
+        <div class="line"></div>
+
+        <div class="contact-send">
+
+        <ul>
+        <li>
+        <input type="text" name="Name"  placeholder="Name *" colspan="4" required />
+        <input type="email" name="Email"  placeholder="Email *" required />
+        <input type="text" name="Subject"  placeholder="Subject"/>
+        </li>
+        <li>
+        <input class="message" type="text" name="Meaasge"  placeholder="Message"/>
+        </li>
+        <button class="button" type="button">Send Message</button>
+        </ul>
+     </div>
+    </div>
+
+     <div class="map">
+     <img src="../img/Map.png">
+     </div>
+</body>
+</html>
