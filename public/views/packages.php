199e33539f7de448f08f93c561f7a9798d22391d
Divia Dennis <divia.deidre@outlook.com>
2017-05-29 11:49:32 -0500

    ...

---
+++ public/views/packages.php
@@ -0,0 +1,49 @@
+<?php $title="Packages"; ?>
+<?php require_once('header.php'); ?> 
+
+<div class="container-fluid">
+<div class="row">
+  <div class="col-md-12">
+    <div class="sub-gallery-nav1">
+      <h1> Packages </h1>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="container-fluid">
+<div class="row">
+
+<div class="p-block-1">
+<div class="col-md-8">
+<h2> Basic Package </h1>
+<ol>
+<div class="p-list-a">
+<li>Pick Up</li>
+<li>Storage</li>
+<li>Embalm</li>
+<li>Preparation</li>
+<li>Casket</li>
+</div>
+<div class="p-list-b">
+<li>Casket Spray</li>
+<li>Hearse</li>
+<li>Vault</li>
+<li>100 Programs</li>
+<li>100 Prayer Cards/Book Markers</li>
+<li>1 TVJ Death Announcement</li>
+</div>
+</ol>
+</div>
+</div>
+
+<div class="p-block-2">
+<div class="col-md-4">
+
+</div>
+</div>
+
+
+</div>
+</div>
