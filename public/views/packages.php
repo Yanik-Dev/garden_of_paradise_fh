ce2e73944c339e4d4c1d02e2fb4b8261842507c4
Divia Dennis <divia.deidre@outlook.com>
2017-05-29 14:41:19 -0500

    Complted Packages Page

--- public/views/packages.php
+++ public/views/packages.php
@@ -12,38 +12,138 @@
 </div>
 
 
+<div class="container">
+            <div class="row">
+
+              <div class="col-md-1 col-sm-2 col-xs-4 float-shadow">
+                </div>
+                    
+                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">        
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Basic Package</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row">Pick Up</div>
+                            <div class="price_table_row">Storage</div>
+                            <div class="price_table_row">Embalm</div>
+                            <div class="price_table_row">Preparation</div>
+                            <div class="price_table_row">Casket</div>
+                            <div class="price_table_row">Casket Spray</div>     
+                            <div class="price_table_row">Hearse</div>   
+                            <div class="price_table_row">Vault</div>     
+                            <div class="price_table_row">100 Programs</div>
+                            <div class="price_table_row">100 Prayer Cards/Book Markers</div>       
+                            <div class="price_table_row">1 TVJ Death Announcement and no Gleaner Advertisment</div>                            
+                        </div>
+                        <a href="#" class="btn btn-primary btn-lg btn-block">Apply For Package</a>
+                    </div>
+                </div>
+        
+                <div class="col-md-4 col-sm-6 col-xs-12 float-shadow">
+                  <div class="recommended"><strong><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> RECOMMENDED</strong></div>        
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Garden Package</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row">Pick Up</div>
+                            <div class="price_table_row">Storage</div>
+                            <div class="price_table_row">Embalm</div>
+                            <div class="price_table_row">Preparation</div>
+                            <div class="price_table_row">Regular or Semi-Customized Casket</div>
+                            <div class="price_table_row">Casket Spray</div>     
+                            <div class="price_table_row">Hearse</div>   
+                            <div class="price_table_row">Vault</div>     
+                            <div class="price_table_row">150 Programs</div>
+                            <div class="price_table_row">150 Prayer Cards/Book Markers</div>       
+                            <div class="price_table_row">2 TVJ Death Announcement or 1 Gleaner Advertisment</div>                            
+                        </div>
+                        <a href="#" class="btn btn-primary btn-lg btn-block">Apply For Package</a>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">        
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Paradise Package</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row">Pick Up</div>
+                            <div class="price_table_row">Storage</div>
+                            <div class="price_table_row">Embalm</div>
+                            <div class="price_table_row">Preparation</div>
+                            <div class="price_table_row">Regular or Fully-Customized Casket</div>
+                            <div class="price_table_row">Casket Spray</div>     
+                            <div class="price_table_row">Hearse</div>   
+                            <div class="price_table_row">Vault and Wreath</div>     
+                            <div class="price_table_row">200 Programs</div>
+                            <div class="price_table_row">200 Prayer Cards/Book Markers</div>       
+                            <div class="price_table_row">2 TVJ Death Announcement and 1 Gleaner Advertisment</div>                            
+                        </div>
+                        <a href="#" class="btn btn-primary btn-lg btn-block">Apply For Package</a>
+                    </div>
+                </div>
+
+        <div class="col-md-1 col-sm-2 col-xs-4 float-shadow">
+      </div>
+
+      </div>
+      </div>
+
+
 <div class="container-fluid">
 <div class="row">
 
-<div class="p-block-1">
-<div class="col-md-8">
-<h2> Basic Package </h1>
-<ol>
-<div class="p-list-a">
-<li>Pick Up</li>
-<li>Storage</li>
-<li>Embalm</li>
-<li>Preparation</li>
-<li>Casket</li>
-</div>
-<div class="p-list-b">
-<li>Casket Spray</li>
-<li>Hearse</li>
-<li>Vault</li>
-<li>100 Programs</li>
-<li>100 Prayer Cards/Book Markers</li>
-<li>1 TVJ Death Announcement</li>
-</div>
-</ol>
-</div>
-</div>
+  <div class="col-md-3">
+  </div>
 
-<div class="p-block-2">
-<div class="col-md-4">
+  <div class="col-md-6">
+    <div class="package-text">
+    <h2> Pre-Funeral Arrangements </h2>
+    <p>Garden of Paradise establish your wishes ahead of time. This considerate act can shield your family from the difficult decision & confusion that often occur when limited time prevents thoughtful planning. We will help to plan your funeral ahead of time for you or your family member. We will explain your options; best of all you can make choices with freedom from the usual urgencies that are created when a death occurs.</p>
+  </div>
+  </div>
+
+   <div class="col-md-3">
+  </div>
 
 </div>
 </div>
 
+<div class="container-fluid">
+<div class="row">
+
+  <div class="col-md-3">
+  </div>
+
+  <div class="col-md-6">
+    <div class="package-text">
+    <h2>Why Should You Pre-Arrange?</h2>
+    <ol>
+    <li>You would like to have your affairs in order.</li>
+    <li>You have definte preference about your funeral service.</li>
+    <li>You want your family to be informed.</li>
+    <li>You would like to have the peace of mind of knowing there are adequate funds to cover funeral expenses.</li>
+    </ol>
+  </div>
+  </div>
+
+   <div class="col-md-3">
+  </div>
+
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+
+  <div class="col-md-3">
+  </div>
+
+  <div class="col-md-6">
+    <div class="package-text">
+    <h2> Pre-Paying (Optional)</h2>
+    <p>This is a very important part of the pre-arrangement as this will allow you to pay for the services at the current prices. This provides protection from inflation and ensures adequate funds for future payment for the service.</p>
+  </div>
+  </div>
+
+   <div class="col-md-3">
+  </div>
 
 </div>
 </div>
