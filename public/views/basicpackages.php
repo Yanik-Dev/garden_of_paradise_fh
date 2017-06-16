ecee496b216e104f219a1fab503ae8f441af6bb4
Divia Dennis <divia.deidre@outlook.com>
2017-06-16 17:28:16 -0500

    table

--- public/views/basicpackages.php
+++ public/views/basicpackages.php
@@ -1,153 +1,309 @@
-<?php $title="Basic Package"; ?>
-<?php require_once('./header.php'); ?> 
+<?php 
+$title = 'Basic Package';
+$items = [];
+if($_GET['type'] =='bp'){
+    $items = [
+        "Pick Up", "Storage","Embalm & Preparation",
+        "Casket","Casket Spray","Hearse","100 Programs","100 Prayer Cards/Book Markers",
+        "TVJ's Death Announcement",
+    ];
+}
+else if($_GET['type'] =='bpp'){
+    $title = 'Basic Personal';
+    $items = [
+        "Pick Up", "2 Weeks Storage","Embalm & Preparation",
+        "Casket","Casket Spray","Hearse","SingleVault (Dovecot or Meadowrest)", 
+        "100 Programs", "100 Prayer Cards or Book Markers",
+        "1 TVJ's Death Announcement",
+    ];
+}
+else if($_GET['type'] =='bpd'){
+     $title = 'Basic Package - Deluxe';
+     $items = [
+        "Pick Up", "2 Weeks Storage","Embalm & Preparation",
+        "Casket","Casket Spray","Hearse","Vault 1/2 Double (Dovecot or Meadowrest)", 
+        "100 Programs", "100 Prayer Cards or Book Markers",
+        "1 TVJ's Death Announcement", "JUTC Bus & 50 Buttons"
+    ];
+}
 
-<div class="container-fluid">
-<div class="row">
-  <div class="col-md-12">
-    <div class="sub-gallery-nav1">
-      <h1> Basic Packages </h1>
-    </div>
-  </div>
-</div>
-</div>
+else if($_GET['type'] =='bpdp'){
+     $title = 'Basic Package - Deluxe Personal';
+     $items = [
+        "Pick Up", "2 Weeks Storage","Embalm & Preparation",
+        "Casket","Casket Spray","Hearse","Single Vault (Dovecot or Meadowrest)", 
+        "100 Programs", "100 Prayer Cards or Book Markers",
+        "1 TVJ's Death Announcement", "JUTC Bus & 50 Buttons"
+    ];
+}
+else if($_GET['type'] =='bps'){
+     $title = 'Basic Package - Super';
+     $items = [
+        "Pick Up", "2 Weeks Storage","Embalm & Preparation",
+        "Casket","Casket Spray + Arrangement","Hearse","Single Vault (Dovecot or Meadowrest)", 
+        "100 Programs", "100 Prayer Cards or Book Markers",
+        "1 TVJ's Death Announcement", "JUTC Bus & 50 Buttons", "Bands for Set UP"
+    ];
+}
+?>
 
-<div class="container">
-            <div class="row">                                
-                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">   
-                 <div class="recommended"><strong><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>BASIC</strong></div>            
-                    <div class="price_table_container">
-                      <div class="price_table_heading">Basic Package</div>
-                        <div class="price_table_body">
-                            <div class="price_table_row">Pick Up</div>
-                            <div class="price_table_row">Storage</div>
-                            <div class="price_table_row">Embalm</div>
-                            <div class="price_table_row">Preparation</div>
-                            <div class="price_table_row">Casket</div>
-                            <div class="price_table_row">Casket Spray</div>     
-                            <div class="price_table_row">Hearse</div>   
-                            <div class="price_table_row">Vault ½ Double (Dovecot or Meadowrest)</div>     
-                            <div class="price_table_row">100 Programs</div>
-                            <div class="price_table_row">100 Prayer Cards/Book Markers</div>       
-                            <div class="price_table_row">1 TVJ Death Announcement and no Gleaner Advertisment</div>                            
-                        </div>
-                        <a href="./request-quote.php?type=bp" class="btn btn-primary btn-lg btn-block">Request Quote</a>
-                    </div>
-                </div>
+ <div class="container">
+	<div class="row">
+	<table class=
+    "plan table table-hover table-bordered table-striped text-center">
+        <thead>
+            <tr>
+                <th colspan="6">
+                    <h2>Features</h2>
+                </th>
 
-                        <div class="col-md-2 col-sm-2 col-xs-4 float-shadow">
-            </div>
-        
-                <div class="col-md-6 col-sm-6 col-xs-12 float-shadow">
+                <th colspan="2">
+                     <h5> Basic Package </h5>
+                </th>
 
-                <div class="added-package-photo">
-               <div class="added-package-photo1">
-               <img src="../assets/img/garden1.png">
-                </div>
-                </div>
+                <th colspan="2">
+                    <h5>Basic Package - Personal</h5>
+                </th>
 
-                <div class="added-package">
-                <h3> Basic Package - Personal </h3>
-                <p>Pick up, 2 Weeks Storage ($2000 per day thereafter), Embalm, Preparation, Hearse, Casket, Casket Spray,
-                  SingleVault (Dovecot or Meadowrest), 100 Programs, 100 Book markers or Prayer cards, 1 TVJ Death Announcement</p>
+                <th colspan="2">
+                    <h5>Basic Package - Deluxe</h5>
+                </th>
 
-                </div>
+                
+                <th colspan="2">
+                    <h5>Basic Package - Deluxe Personal</h5>
+                </th>
 
+                
+                <th colspan="2">
+                    <h5>Basic Package - Super</h5>
+                </th>
+            </tr>
+        </thead>
 
-                <div class="added-package">
-                <h3> Basic Package - Deluxe </h3>
-                <p>Pick up, 2 Weeks Storage ($2000 per day thereafter), Embalm, Preparation, Hearse, Casket, Casket Spray,
-                Vault ½ Double (Dovecot or Meadowrest), 100 Programs, 100 Book markers or prayer cards, 1TVJ Death Announcement,
-                JUTC Bus & 50Buttons</p>
+        <tbody>
+            <tr>
+                <td colspan="6">NUMBER OF
+                LICENSES</td>
 
-                </div>
+                <td colspan="2">1 LICENSE</td>
 
-                <div class="added-package">
-                <h3> Basic Package - Deluxe Personal</h3>
-                <p>Pick up, 2 Weeks Storage ($2000 per day thereafter), Embalm, Preparation, Hearse, Casket, Casket Spray,
-                Single Vault (Dovecot or Meadowrest), 100 Programs, 100 Book markers or prayer cards, 1TVJ Death Announcement,
-                JUTC Bus & 50Buttons</p>
+                <td colspan="2">1 LICENSE</td>
 
-                </div>
+                <td colspan="2">CUSTOM TEAM
+                SIZE</td>
+            </tr>
 
-                <div class="added-package">
-                <h3> Basic Package - Super</h3>
-                <p>Pick up, 2 Weeks Storage ($2000 per day thereafter), Embalm, Preparation, Hearse, Casket, Casket Spray + Arrangement,
-                Single Vault (Dovecot or Meadowrest), 100 Programs, 100 Book markers or prayer cards, 1TVJ Death Announcement,
-                JUTC Bus & 100Buttons, Bands for Set Up</p>
+            <tr>
+                <td colspan="6">UNLIMITED MEETINGS AND ATTENDEES</td>
 
-                </div>
+                <td colspan="2">4 MEETINGS PER MONTH</td>
 
-                </div>
+                <td colspan="2">✔</td>
 
-              
+                <td colspan="2">✔</td>
+            </tr>
 
-        <div class="col-md-1 col-sm-2 col-xs-4 float-shadow">
-      </div>
+            <tr>
+                <td colspan="6">STREAMLINED MEETING PLANNING FUNCTION</td>
 
-      </div>
-      </div>
+                <td colspan="2">✔</td>
 
+                <td colspan="2">✔</td>
 
-<div class="container-fluid">
-<div class="row">
+                <td colspan="2">✔</td>
+            </tr>
 
-  <div class="col-md-3">
-  </div>
+            <tr>
+                <td colspan="6">CLONE PREVIOUS MEETINGS FOR RAPID PLANNING</td>
 
-  <div class="col-md-6">
-    <div class="package-text">
-    <h2> Pre-Funeral Arrangements </h2>
-    <p>Garden of Paradise establish your wishes ahead of time. This considerate act can shield your family from the difficult decision & confusion that often occur when limited time prevents thoughtful planning. We will help to plan your funeral ahead of time for you or your family member. We will explain your options; best of all you can make choices with freedom from the usual urgencies that are created when a death occurs.</p>
-  </div>
-  </div>
+                <td colspan="2">✔</td>
 
-   <div class="col-md-3">
-  </div>
+                <td colspan="2">✔</td>
 
-</div>
-</div>
+                <td colspan="2">✔</td>
+            </tr>
 
-<div class="container-fluid">
-<div class="row">
+            <tr>
+                <td colspan="6">AUTOMATED AGENDA CREATION AND CALENDAR INVITE
+                DISTRIBUTION</td>
 
-  <div class="col-md-3">
-  </div>
+                <td colspan="2">✔</td>
 
-  <div class="col-md-6">
-    <div class="package-text">
-    <h2>Why Should You Pre-Arrange?</h2>
-    <ol>
-    <li>You would like to have your affairs in order.</li>
-    <li>You have definte preference about your funeral service.</li>
-    <li>You want your family to be informed.</li>
-    <li>You would like to have the peace of mind of knowing there are adequate funds to cover funeral expenses.</li>
-    </ol>
-  </div>
-  </div>
+                <td colspan="2">✔</td>
 
-   <div class="col-md-3">
-  </div>
+                <td colspan="2">✔</td>
+            </tr>
 
-</div>
-</div>
+            <tr>
+                <td colspan="6">INTEGRATION WITH EVERNOTE, DROPBOX AND GOOGLE
+                DOCS</td>
 
-<div class="container-fluid">
-<div class="row">
+                <td colspan="2">✔</td>
 
-  <div class="col-md-3">
-  </div>
+                <td colspan="2">✔</td>
 
-  <div class="col-md-6">
-    <div class="package-text">
-    <h2> Pre-Paying (Optional)</h2>
-    <p>This is a very important part of the pre-arrangement as this will allow you to pay for the services at the current prices. This provides protection from inflation and ensures adequate funds for future payment for the service.</p>
-  </div>
-  </div>
+                <td colspan="2">✔</td>
+            </tr>
 
-   <div class="col-md-3">
-  </div>
+            <tr>
+                <td colspan="6">STREAMLINED MEETING EXECUTION FUNCTION</td>
 
-</div>
-</div>
+                <td colspan="2">✔</td>
 
-<?php require_once('footer.php'); ?>
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">EASILY CAPTURE KEY MEETING INFORMATIN (NOTES,
+                ACTION ITEMS, DECISIONS)</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">AUTOMATED CREATION AND DISTRIBUTION OF MEETING
+                SUMMARY REPORT</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">BRANDED MEETING AGENDAS AND SUMMARY REPORTS
+                (PDF)</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">CLOUD SYNC AND ARCHIVE OF MEETING DATA</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">FULL ENCRYPTION OF DATA IN TRANSIT</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">ATTENDEE DASHBOARD FOR PERSONAL VIEW OF MEETING
+                INFO & ASSIGNMENTS</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">AUTOMATED EMAIL DISTRIBUTION OF MEETING
+                ASSIGNMENTS</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">AUTOMATED WORKFLOW TO CAPTURE STATUS UPDATES
+                FOR ASSIGNMENTS</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">ADVANCED FREE FORM TEXT SEARCH ACROSS ALL
+                MEETING DATA</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">PROJECT LEVEL MEETING ANALYTICS WITH OVER 20
+                KEY METRICS</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">EASILY TAG & INFORM OUTSIDE PARTIES ON KEY
+                MEETING INFORMATION</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">COMPLETE ONLINE ACCESS TO ALL MEETING DATA</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">SECURE BACKUP AND RESTORE FROM CLOUD</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+
+            <tr>
+                <td colspan="6">PREMIUM SUPPRORT
+                ACCESS TO ASSIST WITH QUESTIONS & ENHANCEMENT REQUESTS</td>
+
+                <td colspan="2">✗</td>
+
+                <td colspan="2">✔</td>
+
+                <td colspan="2">✔</td>
+            </tr>
+        </tbody>
+    </table>
